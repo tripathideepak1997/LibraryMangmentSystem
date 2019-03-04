9dea5ffcef0f8cf1e2ae1f23e684716c132ef25a
tripathideepak1997 <47346388+tripathideepak1997@users.noreply.github.com>
2019-03-05 01:35:40 +0530

    Update Library.py
    The Do_Validation is not working yet and there are some little bugs that are resolvable.

--- Library.py
+++ Library.py
@@ -1,22 +1,23 @@
 import sqlite3 as sq
-import csv
-import argparse
+import datetime
+# import csv
+# import argparse
 
 
 class Library :
     def __init__(self):
         self.table_creation = '''CREATE TABLE if not exists`Book_Details`  (
-	                                `ISBN`	INTEGER,
+	                                `ISBN`	INTEGER check(ISBN > 999999999 and ISBN < 10000000000),
                                     `Book_Name`	TEXT NOT NULL,
                                     `Author_Name`	TEXT NOT NULL,
-                                    `Count_of_Books`	INTEGER CHECK(Count_of_books > -1),
+                                    `Count_of_Books`	INTEGER CHECK(Count_of_books > 0),
                                     PRIMARY KEY(`ISBN`)
                                     );'''
         self.sqlEntry(self.table_creation)
 
     def viewAll(self,table = 'Book_Details'):
 
-        print("Do you want to view all the records or limit the records : ")
+        print(f"Do you want to view all the records or limit the records of {table} table : ")
         print(" 1: All Records   2: Limit the Records ")
         choice = int(input('Enter the choice : '))
 
@@ -131,6 +132,7 @@ class User :
         self.table_creation = '''CREATE TABLE if not exists`User_Details`  (
     	                                `UserId`	INTEGER,
                                         `User_Name`	TEXT NOT NULL,
+                                        'Fine' Integer default 0,
                                         PRIMARY KEY(`UserId`)
                                         );'''
         self.sqlEntry(self.table_creation)
@@ -267,12 +269,52 @@ class Issue_And_Return_Books(Library,User):
         book_name = input('Enter the book name : ')
         book_isbn = input('Enter the book Isbn : ')
 
-        
 
-        sql_query = "insert into Issue_Status(Issued_User_Id,Issued_Book_Name,Issued_book_ISBN) values (?,?,?) ;"
-        params = (user_id,book_name,book_isbn)
+        sql_query_to_check_the_count = f"Select Count_of_Books from Book_Details where ISBN = {book_isbn}; "
+        data_received = self.sqlEntry(sql_query_to_check_the_count,receive= True)
 
-        self.sqlEntry(sql_query,params)
+        if data_received[0][0] > 0:
+            sql_query_to_decrement = f"Update Book_Details set Count_of_Books = {data_received[0][0] - 1} where ISBN = {book_isbn};"
+            self.sqlEntry(sql_query_to_decrement)
+            sql_query = "insert into Issue_Status(Issued_User_Id,Issued_Book_Name,Issued_book_ISBN) values (?,?,?) ;"
+            params = (user_id,book_name,book_isbn)
+
+            self.sqlEntry(sql_query,params)
+        else:
+            print('The number of books are insufficient wait for their availablity')
+            Library.viewAll(self)
+
+
+    def return_Book(self):
+
+        print('This will return book for you')
+        self.view_Issue()
+
+        user_id = int(input('Enter your user id : '))
+        book_name = input('Enter the book name : ')
+        book_isbn = input('Enter the book Isbn : ')
+
+        #possible,msg = self.do_validation(user_id,book_name,book_isbn)
+
+        #if possible == True:
+
+        sql_query_to_check_the_count = f"Select Count_of_Books from Book_Details where ISBN = {book_isbn} ;"
+        data_received = self.sqlEntry(sql_query_to_check_the_count,receive=True)
+
+        sql_query_to_increment = f"Update Book_Details set Count_of_Books = {data_received[0][0] + 1} where ISBN = {book_isbn};"
+        self.sqlEntry(sql_query_to_increment)
+
+        self.fine_calculation(user_id,book_isbn)
+
+
+        sql_query = "insert into Return_Status(Return_User_Id,Return_Book_Name,Returned_book_ISBN) values (?,?,?) ;"
+        params = (user_id, book_name, book_isbn)
+
+        self.sqlEntry(sql_query, params)
+
+        # else :
+        #     print(f"It is not possible to return {book_name} by {user_id} due to {msg}")
+
 
     def view_Issue(self):
         self.viewAll(table='Issue_Status')
@@ -281,6 +323,66 @@ class Issue_And_Return_Books(Library,User):
     def view_Return(self):
         self.viewAll(table='Return_Status')
 
+    def del_selected(self):
+
+        print(" \n How Do you want to delete in the Issue Status and Return Status table : ")
+        print(" \n 1: Issue Status  \n 2: Return Status  ")
+        choice = int(input("Enter the choice : "))
+
+        if choice == 1:
+            sql_query = "Delete from Issue_Status where Issue_Id = ? ;"
+            issue_id = int(input('Enter the Issue_id you want to delete  : '))
+            params = (issue_id,)
+            self.sqlEntry(sql_query, params)
+
+        if choice == 2:
+            sql_query = "Delete  from Return_Status where Return_Id  = ? ;"
+            return_id = input('Enter the Return_Id you want to delete  : ')
+            params = (return_id,)
+            self.sqlEntry(sql_query, params)
+
+
+    def fine_calculation(self,userid,book_isbn):
+        current_fine = f"Select Fine from User_Details where UserId = {userid}; "
+        data_fine = self.sqlEntry(current_fine,receive=True)
+
+
+        issue_date = f"Select Max(Issue_Date) from Issue_Status where Issued_User_Id = {userid} and Issued_book_ISBN = {book_isbn} ;"
+        date_issue_str = self.sqlEntry(issue_date,receive=True)
+
+        return_date = f"Select Max(Return_Date) from Return_Status where Return_User_Id = {userid} and Returned_book_ISBN = {book_isbn} ;"
+        date_return_str = self.sqlEntry(return_date,receive=True)
+
+        date_issue_time_format = datetime.datetime.strptime(date_issue_str[0][0], '%Y-%m-%d %H:%M:%S')
+        date_return_time_format = datetime.datetime.strptime(date_return_str[0][0], '%Y-%m-%d %H:%M:%S')
+
+
+        timedifference = date_return_time_format - date_issue_time_format
+
+        if timedifference.days > 15 :
+            fine = (timedifference.days - 15 ) * 2
+            sql_query_to_update_fine = f"Update User_Details set fine = {fine} where UserId = {userid} ;"
+            self.sqlEntry(sql_query_to_update_fine)
+
+        else :
+            print(f'You have kept the books for {timedifference.days} days and {timedifference.min} minutes and no fine will be charged for it')
+
+
+
+    def do_validation(self,user_id,book_name,book_isbn):
+        flag = True
+
+        query1_Issue_grt_Return = f"Select count(*) from Issue_Status where Issued_Book_Name = {book_name} and Issued_book_ISBN = {book_isbn} ;"
+        issue_count = self.sqlEntry(query1_Issue_grt_Return,receive=True)
+
+        query2_Issue_grt_Return = f"Select count(*) from Return_Status where Return_Book_Name = {book_name} and Returned_book_ISBN = {book_isbn} "
+        return_count = self.sqlEntry(query2_Issue_grt_Return,receive=True)
+
+        if issue_count[0][0] < return_count[0][0]:
+            flag = False
+            return flag,'The return cannot be done as you are returning an item that has already been returned'
+
+        return flag,'No Error'
 
 
 
@@ -288,64 +390,81 @@ class Issue_And_Return_Books(Library,User):
 
 
 
-
-
-
-
-# lib_object = Library()
-# lib_object = User()
-
-# while True:
-#     print("1: Add Entry \n2: View ALL \n3:Search Entry  \n4: Delete Entry \n5 : Exit" )
-#     choice = int(input('Enter the choice : '))
-#
-#     if choice == 1:
-#         lib_object.add_entry_into_table()
-#
-#     if choice == 2:
-#         lib_object.viewAll()
-#
-#     if choice == 3:
-#         lib_object.searchEntry()
-#
-#     if choice == 4:
-#         lib_object.del_selected()
-#
-#     if choice == 5:
-#         break
-
-
-#
-# while True:
-#     print("1: Add Entry \n2: View ALL \n3:Search Entry  \n4: Delete Entry \n5 : Exit" )
-#     choice = int(input('Enter the choice : '))
-#
-#     if choice == 1:
-#         lib_object.add_entry_into_table()
-#
-#     if choice == 2:
-#         lib_object.viewAll()
-#
-#     if choice == 3:
-#         lib_object.searchEntry()
-#
-#     if choice == 4:
-#         lib_object.del_selected()
-#
-#     if choice == 5:
-#         break
-
-obj = Issue_And_Return_Books()
+lib_obj = Library()
+user_obj = User()
+i_and_r_obj = Issue_And_Return_Books()
 
 while True:
-    choice = int(input('Enter the choice'))
+
+    print('\n 1 : Add Book \n 2: Add User \n 3: Issue And Return Book \n 4:Exit')
+    choice = int(input('Enter the choice : '))
 
     if choice == 1:
-        obj.Issue_Book()
+
+        while True:
+            print("\n For Adding Books \n 1: Add Entry \n2: View ALL \n3:Search Entry  \n4: Delete Entry \n5 : Exit")
+            choice = int(input('Enter the choice : '))
+
+            if choice == 1:
+                lib_obj.add_entry_into_table()
+
+            if choice == 2:
+                lib_obj.viewAll()
+
+            if choice == 3:
+                lib_obj.searchEntry()
+
+            if choice == 4:
+                lib_obj.del_selected()
+
+            if choice == 5:
+                break
 
     if choice == 2:
-        obj.viewAll()
+
+        while True:
+            print("\n For Adding User \n 1: Add Entry \n2: View ALL \n3:Search Entry  \n4: Delete Entry \n5 : Exit")
+            choice = int(input('Enter the choice : '))
+
+            if choice == 1:
+                user_obj.add_entry_into_table()
+
+            if choice == 2:
+                user_obj.viewAll()
+
+            if choice == 3:
+                user_obj.searchEntry()
+
+            if choice == 4:
+                user_obj.del_selected()
+
+            if choice == 5:
+                break
 
     if choice == 3:
-        break
 
+        while True:
+            print("\n For Issuing And Returning Books \n 1: Issue Status \n2: Return Status \n"
+                  "3:Issue Book  \n4: Return Book \n5 : Delete Entry from issue or return \n 6: Exit")
+            choice = int(input('Enter the choice : '))
+
+            if choice == 1:
+                i_and_r_obj.view_Issue()
+
+            if choice == 2:
+                i_and_r_obj.view_Return()
+
+            if choice == 3:
+                i_and_r_obj.Issue_Book()
+
+            if choice == 4:
+                i_and_r_obj.return_Book()
+
+            if choice == 5:
+                i_and_r_obj.del_selected()
+
+            if choice == 6:
+                break
+
+    if choice == 4:
+        break
